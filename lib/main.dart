93b303be764785d0554a438a18d3675b24eab179
Humberto Lima <humberto.lima@trescon.com.br>
2019-03-12 10:59:05 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:lisi_contatos/ui/contact_page.dart';
+import 'package:lisi_contatos/ui/home_page.dart';
+
+void main() {
+  runApp(MaterialApp(
+     home: HomePage(),
+     debugShowCheckedModeBanner: false,
+  ));
+}
